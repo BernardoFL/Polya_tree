870f297e372a04e71d3b756530f34bd482a7918b
Bernardo Flores López <bernardofl@utexas.edu>
2023-02-22 15:54:36 -0600

    Added function to grow one simulation of the forest

--- main.jl
+++ main.jl
@@ -1,6 +1,7 @@
 #### Polya tree
 using DataFrames, Distributions, LinearAlgebra, Plots
 import CSV
+include("tree_struc.jl")
 
 data = DataFrame(CSV.File("/Users/bernardo/Downloads/osr.csv"))
 
@@ -30,7 +31,7 @@ function eval_funcs(funcs, x)
     return res
 end
 
-function inside_area(attempt_h, attempt_v, curvas, α₀, α₁)
+function inside_areas(attempt_h, attempt_v, curvas, α₀, α₁)
     for i in eachindex(curvas)
        if -α₁ * curvas[i](attempt_h) ≤ attempt_v ≤ α₀ * curvas[i](attempt_h)
         return true
@@ -39,6 +40,9 @@ function inside_area(attempt_h, attempt_v, curvas, α₀, α₁)
     return false
 end
 
+#= function get_probs()
+    count(i -> 0<th<1 && -α₁ * curvas[i](attempt_h) ≤ attempt_v ≤ α₀ * curvas[i](attempt_h))
+end =#
 # The first row is the first coordinate
 function get_window(x)
     mins = minimum(x, dims = 1)
@@ -73,7 +77,7 @@ function rarea(n, x, alpha0, alpha1, sigma)
     while i <= n
         attempt_h = [rand(Uniform(hw[1,1]-2*det(sigma), hw[1,2]+2*det(sigma))), rand(Uniform(hw[2,1]-2*det(sigma), hw[2,2]+2*det(sigma)))]
         attempt_v = rand(Uniform(min_wind, mx_wind))
-        if inside_area(attempt_h, attempt_v, curvas, alpha0, alpha1)
+        if inside_areas(attempt_h, attempt_v, curvas, alpha0, alpha1)
                 ver[i] = attempt_v
                 hor[i,:] = attempt_h
                 i += 1
@@ -86,11 +90,11 @@ end
 #@enter rarea(2, [1,1], 1, 1, [1 0; 0 1], true)
 ## samples Dirichlet process from 
 ## @param alpha - the alpha parameter of a Dirichlet process 
-function polya_urn(n, x, α, alpha0, alpha1, sigma)
+function polya_urn(n, x, αₘ, alpha0, alpha1, sigma)
     res = DataFrame(th = zeros(n), h=zeros(n), v=zeros(n), z=zeros(n))
     res[1,:] = rarea(1, x, alpha0, alpha1, sigma)[1,:]
     for i in 2:n
-        if rand() < α/(α + i) # if the Ber(alpha/alpha+n) == 1
+        if rand() < αₘ/(αₘ + i) # if the Ber(alpha/alpha+n) == 1
             res[i,:] = res[sample(1:(i-1)),:]
         else
             res[i,:] = rarea(1, x, alpha0, alpha1, sigma)[1,:]
@@ -100,14 +104,69 @@ function polya_urn(n, x, α, alpha0, alpha1, sigma)
 end
 
 
-function area_prob(A)
-    points = rarea(1000, x, alpha0, alpha1, sigma)
-    points = 1
+"""
+    in_dyadic(x, j, m)
+
+Returns whether x is in the dyadic interval [(j-1)/2^m, j/2^m]
+"""
+function in_dyadic(x, j, m)
+    
+    return (j-1)/2^m <= x <= j/2^m
 end
-polya_urn(5, x, 1, 1, 1, [1 0; 0 1]) 
 
 
-plot_mvpdf(MvNormal([1,1],I))
+"""
+    area_prob(x, j, m, α₀, α₁, α, sigma)
+
+This does these steps:
+    1. Get vectors of bivariate normals
+    2. Sample 5000 points from the DP on the union of the inside_areas
+    3. The output is a vector of size nrow(x), so we initialize it with zeros
+    4. Iterating through each clinical trial:
+     4.1 Store horizontal coordinates, then count how many of the points fall into a the single area
+     4.2 Divide it by the total number of points and store the probability. 
+
+j is the node and m is the level on the tree
+"""
+function area_prob(x, j, m, α₀, α₁, αₘ, sigma)
+    curvas = get_curves(x, sigma) 
+    points = polya_urn(5000, x, αₘ, α₀, α₁, sigma)
+    probs = zeros(size(x, 1))
+    for i in 1:size(x, 1)
+        num1 = count(p -> -α₁ * curvas[i]([p.h, p.v]) ≤ p.z ≤ α₀ * curvas[i]([p.h, p.v]) && in_dyadic(p.th,j,m), eachrow(points))
+        num2 = count(p -> 0 ≤ p.z ≤ α₀ * curvas[i]([p.h, p.v]) && in_dyadic(p.th,j,m), eachrow(points))
+        probs[i] = num1 / (num1 + num2)
+    end
+    return probs
+end
+#@enter area_prob([1 1; 2 2], 1, 3, 1,1,1,1)
+intento = polya_urn(5, x, 1, 1, 1, [1 0; 0 1]) 
+
+#Note: the partition sequence is indeed fixed, rn centered around the Lebesgue measure
+# to do: fix the probs = thing, get matrix of probabilities. Figure out how to trace back the leaves to get the final probabilities.
+##use tree_struc.jl to store the trees
+function grow_trees(nlevels, x, αₘ, α₀, α₁, sigma)
+    #instansiate trees
+    forest = [Tree() for xi in x]
+    for i in 2:nlevels
+        nodes_in_level = findall(x -> x.level == i , forest[1]) 
+        #levels are horizontal, trees vertical
+        probs = [area_prob(xi, j, i, α₀, α₁, αₘ, sigma) for j in nodes_in_level, xi in x]
+        probs = hcat(probs...)
+        for j in nodes_in_level 
+            for tree in eachindex(forest)
+                y = rand(Beta(αₘ*probs[i,tree], αₘ))
+                padre = get_parent(tree, j)
+                add_node!(forest[tree], Node(padre.heigth * y , j, i))
+                add_node!(forest[tree], Node(padre.height * (1-y), j, i))
+            end
+        end
+    end 
+    return forest
+end
+
+##### Testing
+plot_mvpdf(MvNormal([ 1,1],I))
 plot_mvpdf(MvNormal([2, 2], I))
 puntos = polya_urn(1000, [1 1; 2 2], 1, 1, 1, [1 0; 0 1])
 scatter(puntos.h, puntos.v, puntos.z)
