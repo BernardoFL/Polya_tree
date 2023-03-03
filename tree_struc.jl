3162f3b2df9785ec6dd7c282a2e7604242a35ddc
Bernardo Flores López <bernardofl@utexas.edu>
2023-03-03 16:26:18 -0600

    Added constructor for tree with inters and probs

--- tree_struc.jl
+++ tree_struc.jl
@@ -3,56 +3,62 @@
 ##############
 
 # Height is the value of the random density. Nothing for the root.
+# rinter is the value (linter, rinter]
 mutable struct Node
-    height::Float64
+    height::Float
     parent::Union{Int, Nothing}
     children::Vector{Int}
     level::Int
-
+    rinter::Float
     # Constructor with default value, new instansiates a node
-    function Node(height::Float64, parent::Union{Int, Nothing}, level::Int)
-        new(height, parent, Vector{Int}(), level)
+    function Node(height::Float, parent::Union{Int, Nothing}, level::Int, rinter::Float)
+        new(height, parent, Vector{Int}(), level, rinter)
     end
 end
-
+ 
 
 mutable struct Tree
     nodes::Vector{Node}
 
 
-    """
+"""
     Tree(quants::Vector{Float64})
 
-Constructor for a posterior tree. quants is a vector containig [median, q_25, q_75].
+Constructor for a posterior tree. probs is a vector containig the sampled probabilities for the sapmle quantiles
 """
-function Tree(quants::Vector{Float64})
-        tree = new(Vector{Node}())
-        add_node!(tree, quants[1], nothing, 1)
-        add_node!(tree, quants[2], 1)
-        add_node!(tree, quants[3], 1)
+function Tree(probs::Vector{Float64}, inter::Vector{Float64})
+        tree = new(Vector{Node}()) #first split in 2 and then in another 2
+        add_node!(tree, 1, nothing, inter[2]) #prob 1 to be in the root
+        add_node!(tree, probs[1], 1, inter[1])
+        add_node!(tree, probs[2], 1, inter[3])
     end
 end
 
 # i: the node you want to attach a child to
-function add_node!(tree::Tree, height::Float64, i::Union{Int, Nothing})
+function add_node!(tree::Tree, height::Float64, i::Union{Int, Nothing}, rinter::Float)
     i > length(tree.nodes) || throw(BoundsError(tree, i)) # this throws the exception when the condition is met
 
-    # Add the node to the tree. 
-    push!(tree.nodes, Node(height, i, distance_to_root(tree, i)))
+    # If adding a root
+    if isnothing(i)
+        push!(tree.nodes, Node(height, i, 0, rinter))
+    else
+        # Add the node to the tree. 
+        push!(tree.nodes, Node(height, i, tree.nodes[i].level + 1, rinter))
 
-    # Indicate the parent now has a new kid
-    push!(tree.nodes[i].children, length(tree.nodes))
+        # Indicate the parent now has a new kid
+        push!(tree.nodes[i].children, length(tree.nodes))
+    end
 end
 
 ## to add which level each node belongs to
-function distance_to_root(tree, node_index)
-    if tree[node_index].parent == 0  # root node
+#= function distance_to_root(tree, node_index)
+    if isnothing(tree[node_index].parent)  # root node
         return 0
     else
         parent_index = tree[node_index].parent
         return 1 + distance_to_root(tree, parent_index)
     end
-end
+end =#
 
 ## getter for children
 function get_children(tree::Tree, id::Int)
