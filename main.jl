8c63f88862fa41c526089d3f40fce4b3fde5236f
Bernardo Flores López <bernardofl@utexas.edu>
2023-03-14 19:06:57 -0500

    Finished

--- main.jl
+++ main.jl
@@ -3,7 +3,9 @@ using DataFrames, Distributions, LinearAlgebra, Plots
 import CSV
 include("tree_struc.jl")
 
-data = DataFrame(CSV.File("/Users/bernardo/Downloads/osr.csv"))
+#Read data and 
+data = DataFrame(CSV.File("/Users/bernardo/Desktop/latents.csv"))
+select!(data, Not(:Column1))
 
 
 ### Plot a 3d density 
@@ -105,17 +107,6 @@ end
 
 
 """
-    in_dyadic(x, j, m)
-
-Returns whether x is in the dyadic interval [(j-1)/2^m, j/2^m]
-"""
-function in_dyadic(x, j, m)
-    
-    return (j-1)/2^m <= x <= j/2^m
-end
-
-
-"""
     area_prob(x, j, m, α₀, α₁, α, sigma)
 
 This does these steps:
@@ -128,16 +119,22 @@ This does these steps:
 
 j is the node and m is the level on the tree
 """
-function area_prob(x, j, m, α₀, α₁, αₘ, sigma)
-    curvas = get_curves(x, sigma) 
-    points = polya_urn(5000, x, αₘ, α₀, α₁, sigma)
-    probs = zeros(size(x, 1))
-    for i in 1:size(x, 1)
-        num1 = count(p -> -α₁ * curvas[i]([p.h, p.v]) ≤ p.z ≤ α₀ * curvas[i]([p.h, p.v]) && in_dyadic(p.th,j,m), eachrow(points))
-        num2 = count(p -> 0 ≤ p.z ≤ α₀ * curvas[i]([p.h, p.v]) && in_dyadic(p.th,j,m), eachrow(points))
-        probs[i] = num1 / (num1 + num2)
-    end
-    return probs
+function area_prob(curva, points::DataFrame, α₀, α₁, inter::Vector{Float64})
+    length(inter) == 2 || throw(ArgumentError("The interval needs to have two elements"))
+
+    num1 = count(p -> -α₁ * curva([p.h, p.v]) ≤ p.z ≤ α₀ * curva([p.h, p.v]) && inter[1] ≤ p.th ≤ inter[2], eachrow(points))
+    num2 = count(p -> 0 ≤ p.z ≤ α₀ * curva([p.h, p.v]) && inter[1] ≤ p.th ≤ inter[2], eachrow(points))
+    return num1 / (num1 + num2)
+end
+
+
+function area_prob(curva, x, αₘ::Integer, α₀, α₁, inter::Vector{Float64}, sigma)
+    length(inter) == 2 || throw(ArgumentError("The interval needs to have two elements"))
+
+    points = polya_urn(5000, select(x, :Lat1, :Lat2), αₘ, α₀, α₁, sigma)
+    num1 = count(p -> -α₁ * curva([p.h, p.v]) ≤ p.z ≤ α₀ * curva([p.h, p.v]) && inter[1] ≤ p.th ≤ inter[2], eachrow(points))
+    num2 = count(p -> 0 ≤ p.z ≤ α₀ * curva([p.h, p.v]) && inter[1] ≤ p.th ≤ inter[2], eachrow(points))
+    return num1 / (num1 + num2)
 end
 #@enter area_prob([1 1; 2 2], 1, 3, 1,1,1,1)
 intento = polya_urn(5, x, 1, 1, 1, [1 0; 0 1]) 
@@ -145,20 +142,31 @@ intento = polya_urn(5, x, 1, 1, 1, [1 0; 0 1])
 #Note: the partition sequence is indeed fixed, rn centered around the Lebesgue measure
 # to do: fix the probs = thing, get matrix of probabilities. Figure out how to trace back the leaves to get the final probabilities.
 ##use tree_struc.jl to store the trees
-function grow_trees(nlevels, x, αₘ, α₀, α₁, sigma)
+function grow_trees(nlevels, x, αₘ::Function, α₀, α₁, sigma)
     #instansiate trees
-    forest = [Tree() for xi in x]
+    curvas = get_curves(x, sigma)
+    
+    ## Create trees for each thing
+    forest = Vector{Tree}(undef, nrow(x))
+    for i in 1:length(forest)
+        function get_probs(inter, m) 
+            area_prob(curvas[i], x, αₘ(m), α₀, α₁, inter, sigma)
+        end
+        forest[i] = Tree(get_probs, collect(select(x[i,:], :low, :median, :hi)))
+    end
+    forest = [Tree(inter -> area_prob(curvas[i], points, α₀, α₁, inter), collect(select(x[i,:], :low, :median, :hi))) for i in 1:nrow(x)]
     for i in 2:nlevels
-        nodes_in_level = findall(x -> x.level == i , forest[1]) 
+        points = polya_urn(5000, select(x, :Lat1, :Lat2), αₘ, α₀, α₁, sigma) # 1 DP for each level
+
         #levels are horizontal, trees vertical
-        probs = [area_prob(xi, j, i, α₀, α₁, αₘ, sigma) for j in nodes_in_level, xi in x]
-        probs = hcat(probs...)
-        for j in nodes_in_level 
+        #probs = [area_prob(xi, j, i, α₀, α₁, αₘ, sigma) for j in nodes_in_level, xi in x]
+        #probs = hcat(probs...)
             for tree in eachindex(forest)
-                y = rand(Beta(αₘ*probs[i,tree], αₘ))
-                padre = get_parent(tree, j)
-                add_node!(forest[tree], Node(padre.heigth * y , j, i))
-                add_node!(forest[tree], Node(padre.height * (1-y), j, i))
+                nodes_in_level = findall(x -> x.level == i-1, forest[tree])  #find all the nodes to split
+                for j in nodes_in_level 
+                    inter = forest[tree][j].inter
+                    y = area_prob(curvas[tree], points, α₀, α₁, [inter[1], mean(inter)]) # <- add the interval here
+                    split_node!(tree, j, y)
             end
         end
     end 
@@ -174,4 +182,5 @@ scatter(puntos.h, puntos.v, puntos.z)
 
  #Plots.CURRENT_PLOT.nullableplot = nothing
 plot((x,y) -> pdf(MultivariateNormal([0,0],I), [x,y]))
-function sample_space 
+# at the end for the summary get the initial partition back. Probably add the partition as an attribute
+
--- tree_struc.jl
+++ tree_struc.jl
@@ -3,16 +3,16 @@
 ##############
 
 # Height is the value of the random density. Nothing for the root.
-# rinter is the value (linter, rinter]
+# inter is the value (linter, rinter]
 mutable struct Node
     height::Float
     parent::Union{Int, Nothing}
     children::Vector{Int}
     level::Int
-    rinter::Float
+    inter::Vector{Float}
     # Constructor with default value, new instansiates a node
-    function Node(height::Float, parent::Union{Int, Nothing}, level::Int, rinter::Float)
-        new(height, parent, Vector{Int}(), level, rinter)
+    function Node(height::Float, parent::Union{Int, Nothing}, level::Int, inter::Vector{Float})
+        new(height, parent, Vector{Int}(), level, inter)
     end
 end
  
@@ -24,18 +24,29 @@ mutable struct Tree
 """
     Tree(quants::Vector{Float64})
 
-Constructor for a posterior tree. probs is a vector containig the sampled probabilities for the sapmle quantiles
+Constructor for a posterior tree. get_probs is a function that given an interval gets its probability.
 """
-function Tree(probs::Vector{Float64}, inter::Vector{Float64})
-        tree = new(Vector{Node}()) #first split in 2 and then in another 2
-        add_node!(tree, 1, nothing, inter[2]) #prob 1 to be in the root
-        add_node!(tree, probs[1], 1, inter[1])
-        add_node!(tree, probs[2], 1, inter[3])
-    end
+function Tree(get_probs::Function, splits::Vector{Float64})
+    length(splits) != 3 || throw(ArgumentError("Splits need to be [q25, q5, q75]"))
+
+    tree = new(Vector{Node}()) #first split in 2 and then in another 2
+
+    #median
+    add_node!(tree, get_probs([0, splits[2]], 1), nothing, [0, splits[2]]) #prob 1 to be in the root
+    add_node!(tree, get_probs([splits[2], 1], 1), nothing, [splits[2], 1])
+
+    #first half
+    add_node!(tree, get_probs([0, splits[1]], 2), 1, [0, splits[1]])
+    add_node!(tree, get_probs([splits[1], splits[2]], 2), 1, [splits[1], splits[2]])
+
+    #second half
+    add_node!(tree, get_probs([splits[2], splits[3]], 2), 2, [splits[2], splits[3]])
+    add_node!(tree, get_probs([splits[3], 1], 2), 2, [splits[3], 1])
+end
 end
 
 # i: the node you want to attach a child to
-function add_node!(tree::Tree, height::Float64, i::Union{Int, Nothing}, rinter::Float)
+function add_node!(tree::Tree, height::Float64, i::Union{Int, Nothing}, rinter::Vector{Float64})
     i > length(tree.nodes) || throw(BoundsError(tree, i)) # this throws the exception when the condition is met
 
     # If adding a root
@@ -69,4 +80,14 @@ function get_parent(tree::Tree, id::Int)::Node
     tree[tree.nodes[id].parent]
 end
 
+function split_node!(tree::Tree, id::Int, y::Float64)
+    padre = get_parent(tree, id)
+    pint = padre.inter
+    pint0 = [pint[1], mean(pint)]
+    pint1 = [mean(pint), pint[2]]
 
+    add_node!(tree, padre.heigth * y, id, pint0)
+    add_node!(tree, padre.height * (1 - y), id, pint1)
+    ### get parent interval and split it in half
+    ## sample probabilities for children and add them to the tree
+end
